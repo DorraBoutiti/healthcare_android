a2098c116f638a59d108527d9891c24b03a31fba
DorraBoutiti <boutitidorra@gmail.com>
2023-04-25 22:45:23 +0200

    add maps

---
+++ app/src/main/java/com/example/healthcareapp/mapsFragment.java
@@ -0,0 +1,45 @@
+package com.example.healthcareapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class mapsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.fragment_maps, container, false);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.my_map);
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(@NonNull LatLng latLng) {
+                        MarkerOptions makerOptions = new MarkerOptions();
+                        makerOptions.position(latLng);
+                        makerOptions.title(latLng.latitude+" KG "+latLng.longitude);
+                        googleMap.clear();
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,20));
+                        googleMap.addMarker(makerOptions);
+                    }
+                });
+            }
+        });
+        return view;
+    }
+}
